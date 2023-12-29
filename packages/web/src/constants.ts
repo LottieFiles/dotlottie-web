3e72c75050ffffea52e020ae9d9d63b293a5bcca
Abdelrahman Ashraf <a.theashraf@gmail.com>
2023-12-29 13:17:59 +0700

    fix: 🐛 IS_NODE -> cannot read properties of undefined

--- packages/web/src/constants.ts
+++ packages/web/src/constants.ts
@@ -2,13 +2,7 @@
  * Copyright 2023 Design Barn Inc.
  */
 
-/// <reference lib="webworker" />
-
-// eslint-disable-next-line no-var
-declare var self: DedicatedWorkerGlobalScope;
-
 export const IS_BROWSER = typeof window !== 'undefined';
-export const IS_NODE = typeof process !== 'undefined' && process.release.name === 'node';
-export const IS_WEB_WORKER = typeof self !== 'undefined' && typeof self.postMessage === 'function';
+export const IS_NODE = typeof process !== 'undefined';
 export const MS_TO_SEC_FACTOR = 1000;
 export const DEFAULT_BG_COLOR = '#00000000';
