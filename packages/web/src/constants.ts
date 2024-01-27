6fc467648c09a762f228f6f6b8ac005bc981268d
Abdelrahman Ashraf <a.theashraf@gmail.com>
2024-01-27 13:50:17 +0700

    fix: 🐛 animation loop not starting in remix apps

--- packages/web/src/constants.ts
+++ packages/web/src/constants.ts
@@ -3,8 +3,5 @@
  */
 
 export const IS_BROWSER = typeof window !== 'undefined' && typeof window.document !== 'undefined';
-// eslint-disable-next-line no-restricted-globals
-export const IS_WEB_WORKER = typeof self !== 'undefined' && typeof Window === 'undefined';
-export const IS_NODE = typeof global !== 'undefined';
 export const MS_TO_SEC_FACTOR = 1000;
 export const DEFAULT_BG_COLOR = '#00000000';
