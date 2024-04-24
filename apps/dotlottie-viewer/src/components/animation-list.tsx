8981ff86b3010e0c7817d2bd2423c9360887a6e0
Afsal <128362946+afsalz@users.noreply.github.com>
2024-04-24 12:34:24 +0800

    fix: 🐛 marker link issue (#186)

--- apps/dotlottie-viewer/src/components/animation-list.tsx
+++ apps/dotlottie-viewer/src/components/animation-list.tsx
@@ -38,7 +38,10 @@ const AnimationList: React.FC<AnimationListProps> = ({ className = '', ...props 
         url="https://lottie.host/294b684d-d6b4-4116-ab35-85ef566d4379/VkGHcqcMUI.lottie"
       />
       <ListItem name="theming example" url={`${import.meta.env.BASE_URL}theming_example.lottie`} />
-      <ListItem name="marker example" url={`${import.meta.env.BASE_URL}marker-example.lottie`} />
+      <ListItem
+        name="marker example"
+        url={`https://lottie.host/a04c548c-307f-420b-9ba8-e90a4a2efea4/MT9OsNynSw.lottie`}
+      />
     </div>
   );
 };
