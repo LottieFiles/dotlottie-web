79677f98bb727a8ee983f9c04c12b6267943593d
tokitouq <suneeth.since2005@gmail.com>
2024-06-10 16:48:29 +0530

    fix: remove development configs from package

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -5,11 +5,7 @@ import * as preset from 'tsup-preset-solid';
 const presetOptions: preset.PresetOptions = {
   entries: [
     {
-      // entries with '.tsx' extension will have `solid` export condition generated
       entry: 'src/index.ts',
-      // will generate a separate development entry
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      dev_entry: true,
     },
   ],
   // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
