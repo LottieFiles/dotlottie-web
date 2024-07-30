c1e54674fff677d2d3a74801b1a988134eb877c6
samuelOsborne <samu.osborne@gmail.com>
2024-07-30 16:17:29 +0200

    fix: types

--- packages/web/src/core/dotlottie-player.types.ts
+++ packages/web/src/core/dotlottie-player.types.ts
@@ -127,7 +127,7 @@ export interface DotLottiePlayer {
 
 export interface Marker {
   duration: number;
-  name: ArrayBuffer | Uint8Array | Uint8ClampedArray | Int8Array | string;
+  name: string;
   time: number;
 }
 
@@ -136,7 +136,7 @@ export interface Config {
   backgroundColor: number;
   layout: Layout;
   loopAnimation: boolean;
-  marker: ArrayBuffer | Uint8Array | Uint8ClampedArray | Int8Array | string;
+  marker: string;
   mode: Mode;
   segment: VectorFloat;
   speed: number;
