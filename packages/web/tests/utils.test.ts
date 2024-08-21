cd4c70e2e0d659784b1e51b7098652726651f23f
Abdelrahman Ashraf <a.theashraf@gmail.com>
2024-08-21 10:54:17 +0700

    chore: remove unused import in utils.test.ts

--- packages/web/tests/utils.test.ts
+++ packages/web/tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, vi } from 'vitest';
+import { describe, test, expect } from 'vitest';
 
 import { hexStringToRGBAInt, isHexColor, isLottie, isDotLottie, getDefaultDPR } from '../src/utils';
 
