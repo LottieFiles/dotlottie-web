5832fe4329517c2ed076678c81435d72cc9e4cec
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-10-18 23:09:57 +0530

    refactor: packages/solid tsup config

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -1,31 +1,13 @@
-import { defineConfig } from 'tsup';
-// eslint-disable-next-line import/no-namespace
-import * as preset from 'tsup-preset-solid';
+import { defineConfig, type Options } from 'tsup';
 
-const presetOptions: preset.PresetOptions = {
-  entries: [
-    {
-      entry: 'src/index.ts',
-    },
-  ],
-  // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  drop_console: false,
-  // Set to `true` to generate a CommonJS build alongside ESM
-  // cjs: true,
-};
-
-export default defineConfig((config) => {
-  const watching = Boolean(config.watch);
-
-  const parsedOptions = preset.parsePresetOptions(presetOptions, watching);
-
-  if (!watching) {
-    const packageFields = preset.generatePackageExports(parsedOptions);
-
-    // will update ./package.json with the correct export fields
-    preset.writePackageJson(packageFields);
-  }
-
-  return preset.generateTsupOptions(parsedOptions);
-});
+export default defineConfig((options: Options) => ({
+  entry: ['./src/index.ts'],
+  clean: true,
+  dts: true,
+  format: ['esm'],
+  tsconfig: './tsconfig.build.json',
+  external: ['solid-js'],
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  noExternal: Object.keys(require('./package.json').dependencies),
+  ...options,
+}));
