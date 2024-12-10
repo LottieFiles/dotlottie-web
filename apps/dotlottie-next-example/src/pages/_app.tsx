317bdf7c29818c121f7bb8b393345bf5737741d1
Abdelrahman Ashraf <a.theashraf@gmail.com>
2024-12-10 22:38:03 +0700

    chore: add JSX import in _app.tsx

--- apps/dotlottie-next-example/src/pages/_app.tsx
+++ apps/dotlottie-next-example/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import type { JSX } from 'react';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
