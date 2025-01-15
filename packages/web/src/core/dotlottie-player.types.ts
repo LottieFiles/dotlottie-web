9c7bd6659044072413748855cc4190351b87b154
samuelOsborne <samu.osborne@gmail.com>
2025-01-15 11:10:48 +0100

    chore: updated binaries with thorvg-pre

--- packages/web/src/core/dotlottie-player.types.ts
+++ packages/web/src/core/dotlottie-player.types.ts
@@ -1,5 +1,6 @@
-/* eslint-disable @typescript-eslint/no-namespace */
 /* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable typescript-sort-keys/interface */
+/* eslint-disable @typescript-eslint/no-namespace */
 // TypeScript bindings for emscripten-generated code.  Automatically generated at compile time.
 declare namespace RuntimeExports {
   let HEAPF32: unknown;
@@ -17,31 +18,35 @@ declare namespace RuntimeExports {
 interface WasmModule {}
 
 type EmbindString = ArrayBuffer | Uint8Array | Uint8ClampedArray | Int8Array | string;
-export interface VectorFloat {
+export interface ClassHandle {
+  isAliasOf(other: ClassHandle): boolean;
   delete(): void;
+  deleteLater(): this;
+  isDeleted(): boolean;
+  clone(): this;
+}
+export interface VectorFloat extends ClassHandle {
+  size(): number;
   get(_0: number): number | undefined;
   push_back(_0: number): void;
   resize(_0: number, _1: number): void;
   set(_0: number, _1: number): boolean;
-  size(): number;
 }
 
-export interface VectorMarker {
-  delete(): void;
+export interface VectorMarker extends ClassHandle {
+  size(): number;
   get(_0: number): Marker | undefined;
   push_back(_0: Marker): void;
   resize(_0: number, _1: Marker): void;
   set(_0: number, _1: Marker): boolean;
-  size(): number;
 }
 
-export interface VectorString {
-  delete(): void;
+export interface VectorString extends ClassHandle {
+  size(): number;
   get(_0: number): EmbindString | undefined;
   push_back(_0: EmbindString): void;
   resize(_0: number, _1: EmbindString): void;
   set(_0: number, _1: EmbindString): boolean;
-  size(): number;
 }
 
 export interface ModeValue<T extends number> {
@@ -55,100 +60,88 @@ export interface FitValue<T extends number> {
 export type Fit = FitValue<1> | FitValue<3> | FitValue<2> | FitValue<4> | FitValue<5> | FitValue<6>;
 
 export interface Layout {
-  align: VectorFloat;
   fit: Fit;
+  align: VectorFloat;
 }
 
-export interface DotLottiePlayer {
-  activeAnimationId(): string;
-  activeThemeId(): string;
+export interface DotLottiePlayer extends ClassHandle {
+  markers(): VectorMarker;
   animationSize(): VectorFloat;
-  buffer(): unknown;
+  stateMachineFrameworkSetup(): VectorString;
   clear(): void;
-  config(): Config;
-  currentFrame(): number;
-  delete(): void;
-  duration(): number;
-  getLayerBounds(_0: EmbindString): VectorFloat;
-  isComplete(): boolean;
   isLoaded(): boolean;
   isPaused(): boolean;
   isPlaying(): boolean;
   isStopped(): boolean;
-  loadAnimation(_0: EmbindString, _1: number, _2: number): boolean;
+  pause(): boolean;
+  play(): boolean;
+  render(): boolean;
+  stop(): boolean;
+  isComplete(): boolean;
+  resetTheme(): boolean;
+  stateMachineStart(): boolean;
+  stateMachineStop(): boolean;
+  setViewport(_0: number, _1: number, _2: number, _3: number): boolean;
+  loopCount(): number;
+  resize(_0: number, _1: number): boolean;
+  currentFrame(): number;
+  duration(): number;
+  requestFrame(): number;
+  setFrame(_0: number): boolean;
+  seek(_0: number): boolean;
+  totalFrames(): number;
+  segmentDuration(): number;
+  stateMachinePostPointerDownEvent(_0: number, _1: number): number;
+  stateMachinePostPointerUpEvent(_0: number, _1: number): number;
+  stateMachinePostPointerMoveEvent(_0: number, _1: number): number;
+  stateMachinePostPointerEnterEvent(_0: number, _1: number): number;
+  stateMachinePostPointerExitEvent(_0: number, _1: number): number;
+  config(): Config;
+  setConfig(_0: Config): void;
   loadAnimationData(_0: EmbindString, _1: number, _2: number): boolean;
   loadAnimationPath(_0: EmbindString, _1: number, _2: number): boolean;
   loadDotLottieData(_0: EmbindString, _1: number, _2: number): boolean;
-  loadStateMachine(_0: EmbindString): boolean;
-  loadStateMachineData(_0: EmbindString): boolean;
-  loopCount(): number;
+  loadAnimation(_0: EmbindString, _1: number, _2: number): boolean;
   manifestString(): string;
-  markers(): VectorMarker;
-  pause(): boolean;
-  play(): boolean;
-  postBoolEvent(_0: boolean): number;
-  postNumericEvent(_0: number): number;
-  postPointerDownEvent(_0: number, _1: number): number;
-  postPointerEnterEvent(_0: number, _1: number): number;
-  postPointerExitEvent(_0: number, _1: number): number;
-  postPointerMoveEvent(_0: number, _1: number): number;
-  postPointerUpEvent(_0: number, _1: number): number;
-  postSetNumericContext(_0: EmbindString, _1: number): number;
-  postStringEvent(_0: EmbindString): number;
-  render(): boolean;
-  requestFrame(): number;
-  resetTheme(): boolean;
-  resize(_0: number, _1: number): boolean;
-  seek(_0: number): boolean;
-  segmentDuration(): number;
-  setConfig(_0: Config): void;
-  setFrame(_0: number): boolean;
-  setSlots(_0: EmbindString): boolean;
-  setStateMachineBooleanContext(_0: EmbindString, _1: boolean): boolean;
-  setStateMachineNumericContext(_0: EmbindString, _1: number): boolean;
-  setStateMachineStringContext(_0: EmbindString, _1: EmbindString): boolean;
   setTheme(_0: EmbindString): boolean;
   setThemeData(_0: EmbindString): boolean;
-  setViewport(_0: number, _1: number, _2: number, _3: number): boolean;
-  startStateMachine(): boolean;
-  stateMachineFrameworkSetup(): VectorString;
-  stop(): boolean;
-  stopStateMachine(): boolean;
-  totalFrames(): number;
+  setSlots(_0: EmbindString): boolean;
+  activeAnimationId(): string;
+  activeThemeId(): string;
+  stateMachineLoad(_0: EmbindString): boolean;
+  stateMachineLoadData(_0: EmbindString): boolean;
+  stateMachineFireEvent(_0: EmbindString): void;
+  stateMachineSetNumericTrigger(_0: EmbindString, _1: number): boolean;
+  stateMachineSetStringTrigger(_0: EmbindString, _1: EmbindString): boolean;
+  stateMachineSetBooleanTrigger(_0: EmbindString, _1: boolean): boolean;
+  stateMachineGetNumericTrigger(_0: EmbindString): number;
+  stateMachineGetStringTrigger(_0: EmbindString): string;
+  stateMachineGetBooleanTrigger(_0: EmbindString): boolean;
+  getLayerBounds(_0: EmbindString): VectorFloat;
+  stateMachineCurrentState(): string;
+  buffer(): unknown;
 }
 
 export interface Marker {
-  duration: number;
-  name: string;
+  name: EmbindString;
   time: number;
+  duration: number;
 }
 
 export interface Config {
   autoplay: boolean;
+  loopAnimation: boolean;
+  mode: Mode;
+  speed: number;
+  useFrameInterpolation: boolean;
+  segment: VectorFloat;
   backgroundColor: number;
   layout: Layout;
-  loopAnimation: boolean;
   marker: EmbindString;
-  mode: Mode;
-  segment: VectorFloat;
-  speed: number;
   themeId: EmbindString;
-  useFrameInterpolation: boolean;
 }
 
 interface EmbindModule {
-  DotLottiePlayer: {
-    new (_0: Config): DotLottiePlayer;
-  };
-  Fit: {
-    Contain: FitValue<1>;
-    Cover: FitValue<3>;
-    Fill: FitValue<2>;
-    FitHeight: FitValue<5>;
-    FitWidth: FitValue<4>;
-    None: FitValue<6>;
-  };
-  Mode: { Bounce: ModeValue<3>; Forward: ModeValue<1>; Reverse: ModeValue<2>; ReverseBounce: ModeValue<4> };
   VectorFloat: {
     new (): VectorFloat;
   };
@@ -158,8 +151,20 @@ interface EmbindModule {
   VectorString: {
     new (): VectorString;
   };
-  createDefaultConfig(): Config;
+  Mode: { Forward: ModeValue<1>; Reverse: ModeValue<2>; Bounce: ModeValue<3>; ReverseBounce: ModeValue<4> };
+  Fit: {
+    Contain: FitValue<1>;
+    Cover: FitValue<3>;
+    Fill: FitValue<2>;
+    FitWidth: FitValue<4>;
+    FitHeight: FitValue<5>;
+    None: FitValue<6>;
+  };
   createDefaultLayout(): Layout;
+  DotLottiePlayer: {
+    new (_0: Config): DotLottiePlayer;
+  };
+  createDefaultConfig(): Config;
   transformThemeToLottieSlots(_0: EmbindString, _1: EmbindString): string;
 }
 
