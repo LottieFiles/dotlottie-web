be4d57558382d5278262d55e1f9c5461df5f4a4e
Jawish Hameed <jawish@gmail.com>
2026-02-17 09:48:28 +0800

    fix(test): vitest build substitution

--- packages/web/vitest.config.node.ts
+++ packages/web/vitest.config.node.ts
@@ -1,6 +1,17 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig } from 'vitest/config';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'));
+
 export default defineConfig({
+  define: {
+    __PACKAGE_NAME__: JSON.stringify(pkg.name),
+    __PACKAGE_VERSION__: JSON.stringify(pkg.version),
+  },
   test: {
     include: ['tests/**/*.node.{test,spec}.ts'],
   },
