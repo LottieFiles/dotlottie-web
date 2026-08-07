c02f4a2d61cff4e86a83ea5491e100edfe5022fc
theashraf <theashraf@users.noreply.github.com>
2026-08-07 09:21:43 +0000

    deploy: d64d5721f4876aa06fb93f59cd1902e1998769d3

---
+++ playground-local-dotlottie.js
@@ -0,0 +1,1 @@
+import{v as o,w as t}from"./assets/dotlottie-player-CTBTToOj.js";import{x as a}from"./assets/dotlottie-player-CTBTToOj.js";o.setWasmUrl(t);export{o as DotLottie,a as DotLottieWorker};
